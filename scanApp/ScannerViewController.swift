ee32db3b9d44d4fcf54338af91a232e0ad9143a8
samuelraymond23 <samuel.raymond23@yahoo.com>
2018-04-23 17:24:46 -0400

    Segue to product view

---
+++ scanApp/ScannerViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ScannerViewController.swift
+//  scanApp
+//
+//  Created by student on 4/23/18.
+//  Copyright © 2018 Angel Chara'e Mitchell. All rights reserved.
+//
+
+import UIKit
+import BarcodeScanner
+class ScannerViewController: UIViewController, BarcodeScannerCodeDelegate, BarcodeScannerErrorDelegate, BarcodeScannerDismissalDelegate {
+    
+    var newProduct: FoodProduct!
+    
+    @IBOutlet var startScanBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func toPresentScan(_ sender: Any, forEvent event: UIEvent) {
+        let viewController = makeBarcodeScannerViewController()
+        viewController.title = "Barcode Scanner"
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func makeBarcodeScannerViewController() -> BarcodeScannerViewController {
+        let viewController = BarcodeScannerViewController()
+        viewController.codeDelegate = self
+        viewController.errorDelegate = self
+        viewController.dismissalDelegate = self
+        return viewController
+    }
+    
+    func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
+        print("Barcode Data: \(code)")
+        print("Symbology Type: \(type)")
+        
+        APIManager().getProduct(barcode: code) { (food: FoodProduct?, error: Error?) in
+            if let food = food {
+                self.newProduct = food
+                print(food.productName)
+                // if product found, go to product view
+                self.performSegue(withIdentifier: "toProduct", sender: nil)
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            if (self.newProduct != nil) {
+                controller.reset()
+                self.newProduct = nil
+            } else {
+                controller.resetWithError()
+            }
+            
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationViewController = segue.destination as! ProductViewController
+        destinationViewController.product = newProduct
+    }
+    
+    func scanner(_ controller: BarcodeScannerViewController, didReceiveError error: Error) {
+        print(error)
+    }
+    
+    func scannerDidDismiss(_ controller: BarcodeScannerViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
