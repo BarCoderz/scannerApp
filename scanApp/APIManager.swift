b5f64dff435f90199642aac1590cff07e151f75e
kymwithay <hillkymberlee@gmail.com>
2018-04-23 16:53:40 -0400

    finished API Manager

--- scanApp/APIManager.swift
+++ scanApp/APIManager.swift
@@ -14,5 +14,25 @@ class APIManager {
     init() {
         session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
     }
+    
+    func getProduct(barcode: String, completion: @escaping (FoodProduct?, Error?) -> ()) {
+        let url = URL(string: APIManager.baseUrl + "\(barcode).json")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            // This will run when the network request returns
+            if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                if ((dataDictionary["product"] != nil)) {
+                    let dictionary = dataDictionary["product"] as! [String: Any]
+                    let product = FoodProduct.newProduct(dictionary: dictionary)
+                    
+                    completion(product, nil)
+                }
+            } else {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
 }
-
